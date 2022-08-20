482c427e90faf2bc00573d9f2db0ad419b4c4258
X3FF <viacelusdrum@gmail.com>
2022-08-21 03:23:13 +0500

    Удалены ненужные (все) состояния. Добавлена проверка на заполненность полей. Добавлен баннер о не заполненных полях

--- src/components/ChatFrame.jsx
+++ src/components/ChatFrame.jsx
@@ -1,16 +1,8 @@
 import React  from 'react'
-import { useState } from 'react'
 
 const ChatFrame = () => {
 
-        //переменные для реплая
-    let replyAuthor = ''
-    let replyMessage = ''
 
-
-        // состояния для инпута и поля вводы текста
-    const [value, setValue] = useState('')  
-    const [messageText, setMessageText] = useState('')
  
         //сообщения пользователя
     const sendMessage = () =>{
@@ -19,21 +11,53 @@ const ChatFrame = () => {
         let time = new Date();
         let uMessage = document.createElement('div') //создаем div-блок ответа в чате
             uMessage.classList.add('userMessage') //присваиваем ему класс
-        let uMessageName = document.createElement('h4') //добавляем теги для имени пользователя, сообщения и времени отправки
-        let uMessageText = document.createElement('h3')   
+        let uMessageName = document.createElement('h3') //добавляем теги для имени пользователя, сообщения и времени отправки
+        let uMessageText = document.createElement('h4')   
         let uMessageTime = document.createElement('p') 
-            uMessageName.innerText = messageText //текст сообщения
+            uMessageName.innerText = document.querySelector('.messageForm-name').value //имя пользователя
             uMessageTime.innerText = time.getHours() + ':' + time.getMinutes() //получаем время часы:минуты
-            uMessageText.innerText = value //имя пользователя в сообщении
+            uMessageText.innerText = document.querySelector('.messageForm-text').value //сообщение пользователя
+            if (uMessageName.innerText && uMessageText.innerText !='' ){
             chat.append(uMessage) //добавляем в поле новое сообщение
             uMessage.prepend(uMessageName)
             uMessage.append(uMessageText)
             uMessage.append(uMessageTime)
             uMessageText.value = '' //очищаем textarea после отправки сообщения
-            chat.scrollTop = 1000; //автопрокрутка чата к последнему сообщению
-            
+            chat.scrollTop = 9999999; //автопрокрутка чата к последнему сообщению
+            setTimeout( botMessage, 1000)
+             } 
+             else{
+               let error = document.querySelector('.error')
+                    error.style.bottom = '20px'
+                setTimeout(function () {error.style.bottom = '-50px';
+                }, 2000);   
+                
+            }
         
     }
+
+    const botMessage =()=>{
+        let chat = document.querySelector('.chatArea')
+        let time = new Date();
+        let bMessage = document.createElement('div') //создаем div-блок ответа в чате
+            bMessage.classList.add('botMessage') //присваиваем ему класс
+        let bMessageName = document.createElement('h3') //добавляем теги для имени бота, сообщения и времени отправки
+        let bMessageText = document.createElement('h4')   
+        let bMessageTime = document.createElement('p') 
+            bMessageName.innerText = 'Чат-бот' //имя бота
+            bMessageTime.innerText = time.getHours() + ':' + time.getMinutes() //получаем время часы:минуты
+            bMessageText.innerText = 'Случайный ответ №' +Math.floor(Math.random(0, 999) *1000) //сообщение бота
+            chat.append(bMessage) //добавляем в поле новое сообщение
+            bMessage.prepend(bMessageName)
+            bMessage.append(bMessageText)
+            bMessage.append(bMessageTime)
+            chat.scrollTop = 9999999; //автопрокрутка чата к последнему сообщению
+            
+
+
+    }
+
+      
 
     
 
@@ -57,22 +81,22 @@ const ChatFrame = () => {
                 <button><img src="reset-icon.png" alt="" /></button>
             </div>
             
-            <div className='replyText hidden'>     {/* панель реплая изначально скрыта через добавление класса .hidden */}
+            <div className='replyText hidden'>    
                 <h4 className='replyTitle'>Ответ на сообщение:</h4>   
-                <p className="replyAuthor">{replyAuthor}</p>
-                <p className="replyMessage">{replyMessage}</p>
+                <p className="replyAuthor"> </p>
+                <p className="replyMessage"> </p>
             </div>
                 {/* инпут и поле ввода текста для сообщения*/}
-            <input className='messageForm-name' type="text" placeholder='Имя'  onChange= {e => setValue(e.target.value)}/>
+            <input className='messageForm-name' type="text" placeholder='Имя' />
             <textarea className='messageForm-text' 
-                value={messageText} 
+                
                 type="text" cols="30"
-                rows="10" placeholder='Сообщение' 
-                onChange= {e => setMessageText(e.target.value)}>
+                rows="10" placeholder='Сообщение'>
              </textarea>
             <button onClick={sendMessage} className='sendMessage'><img src="sendButton.png" alt="" /></button>
+            
         </div>
-
+        <div className='error'><img src="error-icon.png" alt="" /><span>Проверьте все поля!</span></div>
     </div>
   )
 }
